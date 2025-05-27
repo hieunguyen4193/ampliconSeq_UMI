b1c03b79c0b27032c619c5676e00ae8361a493f6
hieunguyen4193 <work@hieunguyen.de>
2025-05-27 14:17:15 +0200

    update: add loggins

--- run_nextflow_pipeline2.sh
+++ run_nextflow_pipeline2.sh
@@ -40,4 +40,29 @@ nextflow run main.nf \
     --reverse_primer_fa "$reverse_primer_fa" \
     --extract_UMI_from_R1 "${extract_UMI_from_R1_sh}" \
     --add_UMI_to_R1_R2_FASTQS "${add_UMI_to_R1_R2_FASTQs_sh}" \
-    -resume -c ./configs/main.config -w ${workdir}
+    -resume -c ./configs/main.config -w ${workdir} \
+     -with-report "${OUTDIR}/report.html" \
+    -with-timeline "${OUTDIR}/timeline.html" \
+    -with-dag "${OUTDIR}/dag.svg"
+
+# loggings all input params
+echo -e "-----------------------------------------------------------------------------" >> -${OUTDIR}/params.log
+echo -e "NEXTFLOW AMPLICON BISULFITE SEQUENCING PIPELINE\n" >> -${OUTDIR}/params.log
+echo -e "-----------------------------------------------------------------------------" >> -${OUTDIR}/params.log
+echo "Pipeline version: 20250526" >> -${OUTDIR}/params.log
+echo "Pipeline run date: $(date)" >> -${OUTDIR}/params.log
+echo "-----------------------------------------------------------------------------" >> -${OUTDIR}/params.log
+echo "Input parameters:" >> -${OUTDIR}/params.log
+echo "SAMPLE_SHEET: $samplesheet" >> -${OUTDIR}/params.log
+echo "OUTDIR: $OUTDIR" >> -${OUTDIR}/params.log
+echo "BismarkIndex: $BismarkIndex" >> -${OUTDIR}/params.log
+echo "min_family_size_threshold: $min_family_size_threshold" >> -${OUTDIR}/params.log
+echo "consensus_rate: $consensus_rate" >> -${OUTDIR}/params.log
+echo "umi_length: $umi_length" >> -${OUTDIR}/params.log
+echo "umt_distance_threshold: $umt_distance_threshold" >> -${OUTDIR}/params.log
+echo "forward_primer_fa: $forward_primer_fa" >> -${OUTDIR}/params.log
+echo "reverse_primer_fa: $reverse_primer_fa" >> -${OUTDIR}/params.log
+echo "extract_UMI_from_R1: $extract_UMI_from_R1_sh" >> -${OUTDIR}/params.log
+echo "add_UMI_to_R1_R2_FASTQs: $add_UMI_to_R1_R2_FASTQs_sh" >> -${OUTDIR}/params.log
+echo "workdir: $workdir" >> -${OUTDIR}/params.log
+echo -e "-----------------------------------------------------------------------------" >> -${OUTDIR}/params.log
